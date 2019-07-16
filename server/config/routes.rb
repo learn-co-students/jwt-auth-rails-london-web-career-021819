1bfd1ad44ec916145a613e3b21b11863efa75919
schazbot <charlotte.pearce1984@gmail.com>
2019-07-16 14:36:45 +0100

    Done.

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # localhost:3000/api/v1/login
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create]
@@ -7,4 +8,5 @@ Rails.application.routes.draw do
       get '/profile', to: 'users#profile'
     end
   end
+
 end
